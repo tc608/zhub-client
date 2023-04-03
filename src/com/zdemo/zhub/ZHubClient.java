076e8283bf4d20036275c1061b67d0216df7cd4a
梁显优 <237809796@qq.com>
2023-04-03 19:43:43 +0800

    修改：topic 消息接收日志打印级别

--- src/com/zdemo/zhub/ZHubClient.java
+++ src/com/zdemo/zhub/ZHubClient.java
@@ -211,7 +211,7 @@ public class ZHubClient extends AbstractConsumer implements IConsumer, IProducer
                     if ((event = topicQueue.take()) == null) {
                         continue;
                     }
-                    logger.log(Level.INFO, "topic[" + event.topic + "] :" + event.value);
+                    logger.log(Level.FINE, "topic[" + event.topic + "] :" + event.value);
                     accept(event.topic, event.value);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -266,7 +266,7 @@ public class ZHubClient extends AbstractConsumer implements IConsumer, IProducer
                     if ((msg = sendMsgQueue.take()) == null) {
                         continue;
                     }
-                    logger.log(Level.FINEST, "send-msg: [" + msg + "]");
+                    // logger.log(Level.FINEST, "send-msg: [" + msg + "]");
                     writer.write(msg.getBytes());
                     writer.flush();
                 } catch (InterruptedException e) {
