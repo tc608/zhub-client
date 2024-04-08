af448042822583d5c5241d5d24fa725d8266056d
绝尘 <237809796@qq.com>
2024-04-08 22:35:55 +0800

    新增：IType(SHORT、LONG、DOUBLE)

--- src/main/java/tccn/IType.java
+++ src/main/java/tccn/IType.java
@@ -7,13 +7,19 @@ import java.util.Map;
 
 public interface IType {
 
-    TypeToken<String> STRING = new TypeToken<String>() {
+    TypeToken<String> STRING = new TypeToken<>() {
     };
 
-    TypeToken<Integer> INT = new TypeToken<Integer>() {
+    TypeToken<Short> SHORT = new TypeToken<>() {
+    };
+    TypeToken<Integer> INT = new TypeToken<>() {
+    };
+    TypeToken<Long> LONG = new TypeToken<>() {
+    };
+    TypeToken<Double> DOUBLE = new TypeToken<>() {
     };
 
-    TypeToken<Map<String, String>> MAP = new TypeToken<Map<String, String>>() {
+    TypeToken<Map<String, String>> MAP = new TypeToken<>() {
     };
 
     TypeToken<List<Map<String, String>>> LMAP = new TypeToken<List<Map<String, String>>>() {
